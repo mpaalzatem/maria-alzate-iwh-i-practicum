5412334edd33a8d97caec33166a27d1774e4ed11
Maria Paulina Alzate <maria.alzate@sparkon.com>
2026-08-10 12:38:19 -0500

    Add Sparkon styling, fix templates, and add README link

--- index.js
+++ index.js
@@ -1,71 +1,61 @@
+require('dotenv').config();
 const express = require('express');
-const axios = require('axios');
 const app = express();
+const axios = require('axios');
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// Homepage - GET request to retrieve all deals and render them in a table
+app.get('/', async (req, res) => {
+  const url = 'https://api.hubapi.com/crm/v3/objects/deals?properties=name,species,watering_frequency';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
 
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const resp = await axios.get(url, { headers });
+    const records = resp.data.results;
+    res.render('homepage', { title: 'Custom Object Homepage', records });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching records');
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+// Show the form to create a new record
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
 });
-*/
 
+// Process the form and create a new record
+app.post('/update-cobj', async (req, res) => {
+  const url = 'https://api.hubapi.com/crm/v3/objects/deals';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+  const data = {
+    properties: {
+      dealname: req.body.name,
+      name: req.body.name,
+      species: req.body.species,
+      watering_frequency: req.body.watering_frequency
+    }
+  };
+
+  try {
+    await axios.post(url, data, { headers });
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error creating record');
+  }
+});
+
+app.listen(3000, () => console.log('App listening on port 3000'));
